6ba2fee96e9f83e8eedee64c93dae76202c8aa7b
Victor Lesk <vil@dyn1007-233.sm.med.ic.ac.uk>
2015-11-19 17:07:32 +0000

    authentication script done, email validator implemented

--- Pod/Classes/StringExtensions.swift
+++ Pod/Classes/StringExtensions.swift
@@ -24,4 +24,9 @@ public extension String{
         if(pattern == nil){ return nil;}
         return componentsSeparatedByString(pattern!);
     }
+    
+    func isEmail() -> Bool {
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: [.CaseInsensitive]);
+        return regex.firstMatchInString(self, options: [], range: NSMakeRange(0, self.utf8.count)) != nil;
+    }
 }
