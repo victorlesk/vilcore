c42873702134b5567300018b09454d8d1d959c22
Victor Lesk <victorlesk@hotmail.com>
2016-02-15 19:22:05 +0000

    Placeholder about/help views, started on item views

--- Pod/Classes/UIColorExtensions.swift
+++ Pod/Classes/UIColorExtensions.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 public extension UIColor{
     public class func colorWithRGB(hexrgb:Int)->UIColor{
@@ -19,8 +20,14 @@ public extension UIColor{
         let g = CGFloat((hexrgb >> 8) & 0xFF);
         let r = CGFloat((hexrgb >> 16) & 0xFF);
         
-        return UIColor(red: CGFloat(r/255.0), green: (g/255.0), blue: (b/255.0), alpha: 1.0);
+        return UIColor(red: CGFloat(r/256.0), green: (g/256.0), blue: (b/256.0), alpha: 1.0);
+    }
+    
+    public func hexRGBString()->String{
+        let r = Int(round(CGFloat(256.0) * CGColorGetComponents(self.CGColor)[0]));
+        let g = Int(round(CGFloat(256.0) * CGColorGetComponents(self.CGColor)[1]));
+        let b = Int(round(CGFloat(256.0) * CGColorGetComponents(self.CGColor)[2]));
         
-        
+        return String(format:"%02x%02x%02x",r,g,b);
     }
 }
